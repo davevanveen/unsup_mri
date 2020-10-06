f5d72cbfe0448349f68cec2f09006778e19bb831
davevanveen <davemvanveen@gmail.com>
2020-10-05 17:32:46 -0700

    gpg

--- utils/data_io.py
+++ utils/data_io.py
@@ -45,12 +45,12 @@ def get_path_loss_curve(file_id, NUM_ITER, DC_STEP, ALPHA):
     ALPHA = check_alpha(DC_STEP, ALPHA)
 
     suffix = '{}_iter{}_alpha{}.npy'.format(file_id, NUM_ITER, ALPHA)
-    fn_mse_wrt_ksp = '{}mse_wrt_ksp_{}'.format(path_out, suffix)
-    fn_mse_wrt_img = '{}mse_wrt_img_{}'.format(path_out, suffix)
+    fn_loss_ksp = '{}mse_wrt_ksp_{}'.format(path_out, suffix)
+    fn_loss_img = '{}mse_wrt_img_{}'.format(path_out, suffix)
 
-    return fn_mse_wrt_ksp, fn_mse_wrt_img
+    return fn_loss_ksp, fn_loss_img
 
-def save_output(img_dc, mse_wrt_ksp, mse_wrt_img, \
+def save_output(img_dc, loss_ksp, loss_img, \
                 file_id, NUM_ITER, DC_STEP, ALPHA):
     ''' given output img_dc and expmt params
         save if it doesnt already exist '''
@@ -58,10 +58,10 @@ def save_output(img_dc, mse_wrt_ksp, mse_wrt_img, \
     np.save(full_path, img_dc)
     print('generated {}'.format(full_path))
 
-    fn_mse_wrt_ksp, fn_mse_wrt_img = get_path_loss_curve(\
+    fn_loss_ksp, fn_loss_img = get_path_loss_curve(\
                                 file_id, NUM_ITER, DC_STEP, ALPHA)
-    np.save(fn_mse_wrt_ksp, mse_wrt_ksp)
-    np.save(fn_mse_wrt_img, mse_wrt_img)
+    np.save(fn_loss_ksp, loss_ksp)
+    np.save(fn_loss_img, loss_img)
 
     return
 
@@ -79,5 +79,11 @@ def expmt_already_generated(file_id, NUM_ITER, DC_STEP, ALPHA):
 def load_output(file_id, NUM_ITER, DC_STEP, ALPHA):
     ''' given expmt specs, load previously generated output file '''
 
-    full_path = get_path_out(file_id, NUM_ITER, DC_STEP, ALPHA)
-    return np.load(full_path)
+    # get reconstructed img
+    path_out = get_path_out(file_id, NUM_ITER, DC_STEP, ALPHA)
+    
+    # get loss curvers
+    path_loss_ksp, path_loss_img = get_path_loss_curve(\
+                            file_id, NUM_ITER, DC_STEP, ALPHA)
+
+    return np.load(path_out), np.load(path_loss_ksp), np.load(path_loss_img)
