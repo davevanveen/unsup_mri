d5f380fa892226009c39e480894fe94bf03c98ae
davevanveen <davemvanveen@gmail.com>
2020-11-10 17:47:05 -0800

    run over qdess at various accel

--- run_qdess_expmt.py
+++ run_qdess_expmt.py
@@ -24,23 +24,27 @@ if torch.cuda.is_available():
     torch.cuda.set_device(1)
 
 
+def load_mask(accel, C=32):
+    ''' load pre-made mask according to accel factor
+        force central CxC pixels in mask to be 1 '''
+
+    mask = torch.from_numpy(np.load('ipynb/mask_poisson_disc_{}x.npy'.format(accel)))
+    mask = abs(mask).type(torch.uint8)
+    idx_y, idx_z = mask.shape[0] // 2, mask.shape[1] // 2
+    mask[idx_y-C:idx_y+C, idx_z-C:idx_z+C] = 1
+
+    return mask
+
+
 def run_expmt():
 
     path_in = '/bmrNAS/people/arjun/data/qdess_knee_2020/files_recon_calib-16/'
-    #path_in = '/bmrNAS/people/dvv/in_qdess/'
     files = [f for f in listdir(path_in) if isfile(join(path_in, f))]
     files.sort()
-    NUM_SAMPS = 25 # number of samples to recon
+    NUM_SAMPS = 10 # number of samples to recon
        
     NUM_ITER = 10000
-    path_out = '/bmrNAS/people/dvv/out_qdess/'
-
-    # load mask, force central CxC pixels in mask to be 1
-    mask = torch.from_numpy(np.load('ipynb/mask_3d.npy'))
-    mask = abs(mask).type(torch.uint8)
-    idx_y, idx_z = mask.shape[0] // 2, mask.shape[1] // 2
-    C = 32
-    mask[idx_y-C:idx_y+C, idx_z-C:idx_z+C] = 1
+    ACCEL_LIST = [6,8]
 
     for fn in files[:NUM_SAMPS]:
 
@@ -53,42 +57,45 @@ def run_expmt():
             f.close()
             continue
         f.close()
-        #fn_npy_in = '/bmrNAS/people/dvv/in_qdess/{}_kspace.npy'.format(fn.split('.h5')[0])
-        #ksp = torch.from_numpy(np.load(fn_npy_in))
         ksp_vol = ksp[:,:,:,0,:].permute(3,0,1,2) # get echo1, reshape to be (nc, kx, ky, kz)
 
         # get central slice in kx, i.e. axial plane b/c we undersample in (ky, kz)
         idx_kx = ksp_vol.shape[1] // 2
         ksp_orig = ksp_vol[:, idx_kx, :, :]
 
-        # initialize network
-        net, net_input, ksp_orig_ = init_convdecoder(ksp_orig, mask)
+        for ACCEL in ACCEL_LIST:
+            
+            path_out = '/bmrNAS/people/dvv/out_qdess/accel_{}x/'.format(ACCEL)
+            mask = load_mask(ACCEL, C=32)
 
-        # apply mask after rescaling k-space. want complex tensors dim (nc, ky, kz)
-        ksp_masked = ksp_orig_ * mask
-        img_masked = ifft_2d(ksp_masked)
+            # initialize network
+            net, net_input, ksp_orig_ = init_convdecoder(ksp_orig, mask)
 
-        # fit network, get net output
-        net, mse_wrt_ksp, mse_wrt_img = fit(
-            ksp_masked=ksp_masked, img_masked=img_masked,
-            net=net, net_input=net_input, mask2d=mask, num_iter=NUM_ITER)
-        img_out = net(net_input.type(dtype))[0] # real tensor dim (2*nc, kx, ky)
-        img_out = reshape_adj_channels_to_complex_vals(img_out) # complex tensor dim (nc, kx, ky)
-        
-        # perform dc step
-        ksp_est = fft_2d(img_out)
-        ksp_dc = torch.where(mask, ksp_masked, ksp_est) # dc step
+            # apply mask after rescaling k-space. want complex tensors dim (nc, ky, kz)
+            ksp_masked = ksp_orig_ * mask
+            img_masked = ifft_2d(ksp_masked)
 
-        # create data-consistent, ground-truth images from k-space
-        img_dc = root_sum_squares(ifft_2d(ksp_dc)).detach()
-        img_gt = root_sum_squares(ifft_2d(ksp_orig))
+            # fit network, get net output
+            net, mse_wrt_ksp, mse_wrt_img = fit(
+                ksp_masked=ksp_masked, img_masked=img_masked,
+                net=net, net_input=net_input, mask2d=mask, num_iter=NUM_ITER)
+            img_out = net(net_input.type(dtype))[0] # real tensor dim (2*nc, kx, ky)
+            img_out = reshape_adj_channels_to_complex_vals(img_out) # complex tensor dim (nc, kx, ky)
+            
+            # perform dc step
+            ksp_est = fft_2d(img_out)
+            ksp_dc = torch.where(mask, ksp_masked, ksp_est) # dc step
 
-        # save results
-        samp = fn.split('.h5')[0] 
-        np.save('{}{}_dc.npy'.format(path_out, samp), img_dc)
-        np.save('{}{}_gt.npy'.format(path_out, samp), img_gt)
+            # create data-consistent, ground-truth images from k-space
+            img_dc = root_sum_squares(ifft_2d(ksp_dc)).detach()
+            img_gt = root_sum_squares(ifft_2d(ksp_orig))
 
-        print('recon {} w shape {}'.format(samp, ksp_vol.shape)) 
+            # save results
+            samp = fn.split('.h5')[0] 
+            np.save('{}{}_dc.npy'.format(path_out, samp), img_dc)
+            np.save('{}{}_gt.npy'.format(path_out, samp), img_gt)
+
+            print('recon {} w shape {}'.format(samp, ksp_vol.shape)) 
 
     return
 
